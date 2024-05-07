5a8709f408ac53ee044f129f8cfbe50c450552e3
lirahunch <147822936+lirahunch@users.noreply.github.com>
2024-05-07 22:26:29 +0400

    Update ThomasNavBar.jsx

--- src/ThomasNavBar.jsx
+++ src/ThomasNavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaMoon, FaSun, FaVolumeMute } from "react-icons/fa";
+import { FaMoon, FaSun, FaVolumeMute, FaTwitter, FaTelegramPlane } from "react-icons/fa";
 
 const ThomasNavBar = ({ dark, setDark, setIsMuted, isMuted }) => {
   return (
@@ -8,14 +8,32 @@ const ThomasNavBar = ({ dark, setDark, setIsMuted, isMuted }) => {
         dark && "animate-bounce"
       }`}
     >
-      <button
-        onClick={() => setIsMuted(!isMuted)}
-        className="p-2 bg-gray-700 rounded hover:bg-gray-600 transition"
-      >
-        <FaVolumeMute />
-      </button>
-
-
+      <div className="flex gap-2">
+        <button
+          onClick={() => setIsMuted(!isMuted)}
+          className="p-2 bg-gray-700 rounded hover:bg-gray-600 transition"
+        >
+          <FaVolumeMute />
+        </button>
+        {/* Twitter Button */}
+        <a
+          href="https://twitter.com/ThomasTankSOL"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-2 bg-gray-700 rounded hover:bg-gray-600 transition"
+        >
+          <FaTwitter />
+        </a>
+        {/* Telegram Button */}
+        <a
+          href="https://t.me/ThomasTank_Portal"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="p-2 bg-gray-700 rounded hover:bg-gray-600 transition"
+        >
+          <FaTelegramPlane />
+        </a>
+      </div>
       <button
         className="p-2 bg-gray-700 rounded hover:bg-gray-600 transition"
         onClick={() => setDark(!dark)}
